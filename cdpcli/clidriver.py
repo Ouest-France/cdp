79530022552ff576ad3774e27cdee394bb1f7a92
Lucas POUZAC <lucas.pouzac.pro@gmail.com>
2018-01-17 11:59:13 +0100

    Improve ingress name

--- cdpcli/clidriver.py
+++ cdpcli/clidriver.py
@@ -95,11 +95,10 @@ def __k8s():
     # Get k8s namespace
     if opt['--namespace-project-name']:
         namespace = os.environ['CI_PROJECT_NAME']
+        host = "%s.%s" % (os.environ['CI_PROJECT_NAME'], os.getenv('DNS_SUBDOMAIN', 'example.com'))
     else:
-        namespace = "%s-%s" % (os.environ['CI_PROJECT_NAME'], os.environ['CI_COMMIT_REF_NAME'])
-
-    # Get deployment host
-    host = "%s.ingress.%s" % (os.environ['CI_PROJECT_NAME'], "example.com") # TODO
+        namespace = "%s-%s" % (os.environ['CI_PROJECT_NAME'], os.environ['CI_COMMIT_REF_NAME'])    # Get deployment host
+        host = "%s.%s.%s" % (os.environ['CI_ENVIRONMENT_SLUG'], os.environ['CI_PROJECT_NAME'], os.getenv('DNS_SUBDOMAIN', 'example.com'))
 
     # Get image tag name
     image_name = __getImageName(__getLoginCmd())
