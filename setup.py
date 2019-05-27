97626009d7ffa4c56b911b02c49e70f8c0778f5a
Lucas POUZAC <lucas.pouzac@lpoconseil.fr>
2019-05-27 11:34:41 +0200

    Add log yaml template

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def find_version(*file_paths):
 requires = ['docopt==0.6.2',
             'python-gitlab==1.8.0',
             'verboselogs==1.7',
-            'pyyaml==4.2b2']
+            'ruamel.yaml==0.15.96']
 
 
 if sys.version_info[:2] == (2, 6):
