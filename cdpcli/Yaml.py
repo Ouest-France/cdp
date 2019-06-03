838b7cb1173c34b55f22c6d4bfbe57b49728af41
Lucas POUZAC <lucas.pouzac@lpoconseil.fr>
2019-06-03 12:03:39 +0200

    Fix parameters

--- cdpcli/Yaml.py
+++ cdpcli/Yaml.py
@@ -3,11 +3,12 @@ from ruamel.yaml import YAML
 from ruamel.yaml.compat import StringIO
 
 class Yaml(YAML):
-    def dump_all(self, data, stream=None, _kw=None, transform=None):
+
+    def dump_all(self, data, stream=None, *args, **kwargs):
         inefficient = False
         if stream is None:
             inefficient = True
             stream = StringIO()
-        YAML.dump_all(self, data, stream, _kw=_kw, transform=transform)
+        YAML.dump_all(self, data, stream, *args, **kwargs)
         if inefficient:
             return stream.getvalue()
