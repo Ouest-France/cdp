afba9a52af3fcb8e9bd72865bcdc7779d0216106
Bastien <bastien.balaud@ouest-france.fr>
2019-09-04 14:25:49 +0200

    fix test

--- cdpcli/clidriver.py
+++ cdpcli/clidriver.py
@@ -564,8 +564,8 @@ class CLIDriver(object):
         if self._context.opt['--release-project-branch-name']:
             # https://github.com/kubernetes/helm/issues/1528
             return self.__getName(False)[:53]
-        elif self._context.opt['--release-project-env-name']
- :           return self.__getEnvName()[:53]
+        elif self._context.opt['--release-project-env-name']:
+            return self.__getEnvName()[:53]
         elif self._context.opt['--release-custom-name']:
             return  self.__getNamespace()[:53]+'-'+self._context.opt['--release-custom-name']
         else:
@@ -577,7 +577,7 @@ class CLIDriver(object):
             name = os.environ['CI_PROJECT_NAME']
         else:
             # Get first letter for each word
-            projectFistLetterEachWord = ''.join([word if len(word) == 0 else word[0] for word in re.split('[^a-zA-Z\d]', os.environ['CI_PROJECT_NAME'])])
+            projectFistLetterEachWord = ''.join([word if len(word) == 0 else word[0] for word in re.split('[^a-zA-Z0-9]', os.environ['CI_PROJECT_NAME'])])
             name = '%s%s-%s' % (projectFistLetterEachWord, os.environ['CI_PROJECT_ID'], os.getenv('CI_COMMIT_REF_SLUG', os.environ['CI_COMMIT_REF_NAME']))    # Get deployment host
 
         return name.replace('_', '-')
@@ -586,7 +586,7 @@ class CLIDriver(object):
         # Get k8s namespace
         if(self.__getEnvironmentName() is not None):
             # Get first letter for each word
-            projectFistLetterEachWord = ''.join([word if len(word) == 0 else word[0] for word in re.split('[^a-zA-Z\d]', os.environ['CI_PROJECT_NAME'])])
+            projectFistLetterEachWord = ''.join([word if len(word) == 0 else word[0] for word in re.split('[^a-zA-Z0-9]', os.environ['CI_PROJECT_NAME'])])
             name = '%s%s-env-%s' % (projectFistLetterEachWord, os.environ['CI_PROJECT_ID'], self.__getEnvironmentName().replace('/', '-'))    # Get deployment host
         elif(self.__getEnvironmentName() is None):
             LOG.err('can not use environnement release option because environment is not defined in gitlab job.')
