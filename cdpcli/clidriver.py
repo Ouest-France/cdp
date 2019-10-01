cecfe9a59e7daba42aa0c7ea49805bef2821c2b0
Bastien BALAUD <bastien.balaud@gmail.com>
2019-10-01 17:22:03 +0200

    fix string cast

--- cdpcli/clidriver.py
+++ cdpcli/clidriver.py
@@ -442,13 +442,13 @@ class CLIDriver(object):
                         self._cmd.run_command('cp /cdp/k8s/secret/cdp-filesecret.yaml %s/templates/' % self._context.opt['--deploy-spec-dir'])
                         secretCustomFileCreated = True
                     # For each envVar of the right environnement we had a line in the secret
-<<<<<<< HEAD
                     self._cmd.run_secret_command('echo "  %s : |" >> %s/templates/cdp-filesecret.yaml' % (envVar, self._context.opt['--deploy-spec-dir']))
                     with open(envValue, "r") as myfile:
                         data = myfile.read()
                     for line in data.splitlines():
                         self._cmd.run_secret_command('echo "    %s" >> %s/templates/cdp-filesecret.yaml' % (line, self._context.opt['--deploy-spec-dir']))
                     LOG.warn(self._cmd.run_secret_command('cat %s/templates/cdp-filesecret.yaml' % (self._context.opt['--deploy-spec-dir'])) )
+
 
         command = '%s --debug' % command
         command = '%s -i' % command
