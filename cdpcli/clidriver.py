b635cad52bd7007aca0315d9447fcc1c54797bcf
Bastien BALAUD <bastien.balaud@gmail.com>
2019-10-22 17:53:03 +0200

    add env var

--- cdpcli/clidriver.py
+++ cdpcli/clidriver.py
@@ -473,7 +473,12 @@ class CLIDriver(object):
                     final_docs.append(doc)
                     #Manage Deployement and
                     if os.getenv('MONITORING'):
-                        doc=CLIDriver.addMonitoringLabel(doc,True)
+                        if os.getenv('CDP_MONITORING') == True:
+                            if os.getenv('CDP_ALERTING') == True:
+                                doc = CLIDriver.addMonitoringLabel(doc, True)
+                            else:
+                                doc = CLIDriver.addMonitoringLabel(doc, False)
+
                     if not self._context.opt['--use-aws-ecr'] and not self._context.opt['--use-registry'] == 'aws-ecr' and 'kind' in doc and  'spec' in doc and ('template' in doc['spec'] or 'jobTemplate' in doc['spec']):
                         doc=CLIDriver.addImageSecret(doc,image_pull_secret_value)
 
