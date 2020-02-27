e3d29828cf31768e3de21b12131ef0208c5c4403
Bastien BALAUD <bastien.balaud@gmail.com>
2020-02-27 16:43:03 +0100

    fix conditionnal

--- cdpcli/Context.py
+++ cdpcli/Context.py
@@ -151,6 +151,6 @@ class Context(object):
 
     def __login(self, registry, registry_user, registry_token):
         # Activate login, only specific stage.
-        if self._opt['maven'] or self._opt['docker'] or (self._opt['k8s'] and "CDP_TAG_PREFIX " in os.environ):
+        if self._opt['maven'] or self._opt['docker'] or (self._opt['k8s'] and "CDP_TAG_PREFIX" in os.environ):
             if registry_user is not None and registry_token is not None and registry is not None:
                 self._cmd.run_command('docker login -u %s -p %s https://%s' % (registry_user, self.string_protected(registry_token), registry))
