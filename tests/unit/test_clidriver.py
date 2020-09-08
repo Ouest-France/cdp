5b8d83a00338be0692b051c533b4af7dfe43363a
Jérémy CHOMAT <prestasin47@ouest-france.fr>
2020-09-08 11:06:37 +0200

    skip create namespace for debug

--- tests/unit/test_clidriver.py
+++ tests/unit/test_clidriver.py
@@ -900,7 +900,7 @@ status:
                         int_file,
                         namespace,
                         final_deploy_spec_dir), 'volume_from' : 'k8s', 'output': 'unnecessary', 'docker_image': TestCliDriver.image_name_helm},
-                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --wait --atomic --description deletionTimestamp=%s'
+                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --create-namespace --wait --atomic --description deletionTimestamp=%s'
                     % (release,
                         final_deploy_spec_dir,
                         namespace,
@@ -984,7 +984,7 @@ status:
                 {'cmd': cmdcurl, 'output': 'unnecessary'},                
                 {'cmd': 'docker pull %s' % TestCliDriver.image_name_conftest, 'output': 'unnecessary'},
                 {'cmd': 'test --policy policy --data data all_resources.yaml', 'volume_from' : 'k8s', 'output': 'unnecessary', 'docker_image': TestCliDriver.image_name_conftest,'workingDir':chartdir},
-                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --wait --atomic --description deletionTimestamp=%s'
+                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --create-namespace --wait --atomic --description deletionTimestamp=%s'
                     % (release,
                         final_deploy_spec_dir,
                         namespace,
@@ -1134,7 +1134,7 @@ status:
                         int_file,
                         namespace,
                         final_deploy_spec_dir), 'volume_from' : 'k8s', 'output': 'unnecessary', 'docker_image': TestCliDriver.image_name_helm},
-                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --wait --atomic --description deletionTimestamp=%s'
+                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --create-namespace --wait --atomic --description deletionTimestamp=%s'
                     % (release,
                         final_deploy_spec_dir,
                         namespace,
@@ -1208,7 +1208,7 @@ status:
                         int_file,
                         namespace,
                         final_deploy_spec_dir), 'volume_from' : 'k8s', 'output': 'unnecessary', 'docker_image': TestCliDriver.image_name_helm},
-                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --wait --atomic --description deletionTimestamp=%s'
+                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --create-namespace --wait --atomic --description deletionTimestamp=%s'
                     % (release,
                         final_deploy_spec_dir,
                         namespace,
@@ -1288,7 +1288,7 @@ status:
                               final_deploy_spec_dir), 'volume_from': 'k8s', 'output': 'unnecessary',
                     'docker_image': TestCliDriver.image_name_helm},
                 {
-                    'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --wait --atomic --description deletionTimestamp=%s'
+                    'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --create-namespace --wait --atomic --description deletionTimestamp=%s'
                            % (release,
                               final_deploy_spec_dir,
                               namespace,
@@ -1369,7 +1369,7 @@ status:
                         int_file,
                         namespace,
                         final_deploy_spec_dir), 'volume_from' : 'k8s', 'output': 'unnecessary', 'docker_image': TestCliDriver.image_name_helm},
-                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --wait --atomic --description deletionTimestamp=%s'
+                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --create-namespace --wait --atomic --description deletionTimestamp=%s'
                     % (release,
                         final_deploy_spec_dir,
                         namespace,
@@ -1440,7 +1440,7 @@ status:
                         int_file,
                         namespace,
                         final_deploy_spec_dir), 'volume_from' : 'k8s', 'output': 'unnecessary', 'docker_image': TestCliDriver.image_name_helm},
-                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --wait --atomic'
+                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --create-namespace --wait --atomic'
                     % (release,
                         final_deploy_spec_dir,
                         namespace)
@@ -1502,7 +1502,7 @@ status:
                         values,
                         namespace,
                         final_deploy_spec_dir), 'volume_from' : 'k8s', 'output': 'unnecessary', 'docker_image': TestCliDriver.image_name_helm},
-                {'cmd': 'upgrade %s %s --timeout %ss --history-max 20 -i --namespace=%s --wait --atomic --description deletionTimestamp=%s'
+                {'cmd': 'upgrade %s %s --timeout %ss --history-max 20 -i --namespace=%s --create-namespace --wait --atomic --description deletionTimestamp=%s'
                     % (release,
                         final_deploy_spec_dir,
                         timeout,
@@ -1569,7 +1569,7 @@ status:
                         TestCliDriver.ci_commit_sha,
                         namespace,
                         final_deploy_spec_dir), 'volume_from' : 'k8s', 'output': 'unnecessary', 'docker_image': TestCliDriver.image_name_helm},
-                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --wait --atomic'
+                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --create-namespace --wait --atomic'
                     % (release,
                         final_deploy_spec_dir,
                         namespace)
@@ -1645,7 +1645,7 @@ status:
                         TestCliDriver.ci_commit_sha,
                         namespace,
                         final_deploy_spec_dir), 'volume_from' : 'k8s', 'output': 'unnecessary', 'docker_image': TestCliDriver.image_name_helm},
-                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --wait --atomic'
+                {'cmd': 'upgrade %s %s --timeout 600s --history-max 20 -i --namespace=%s --create-namespace --wait --atomic'
                     % (release,
                         final_deploy_spec_dir,
                         namespace), 'volume_from' : 'k8s', 'output': 'unnecessary', 'docker_image': TestCliDriver.image_name_helm},
